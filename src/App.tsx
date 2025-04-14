140e6fdc1e12c803ec0689e1b8d7bc8de92784a9
networkerman <networker.udayan@gmail.com>
2025-04-15 00:48:43 +0530

    fix: Remove unused react-query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -10,30 +9,26 @@ import { motion, AnimatePresence } from "framer-motion";
 import { CustomCursor } from "./components/CustomCursor";
 import { pageTransition } from "./lib/animations";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <CustomCursor />
-      <AnimatePresence mode="wait">
-        <motion.div
-          className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white"
-          {...pageTransition}
-        >
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </motion.div>
-      </AnimatePresence>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <CustomCursor />
+    <AnimatePresence mode="wait">
+      <motion.div
+        className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white"
+        {...pageTransition}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </motion.div>
+    </AnimatePresence>
+  </TooltipProvider>
 );
 
 export default App;
